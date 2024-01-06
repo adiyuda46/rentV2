f5cf2278263f22a65cfa8ac6a37b95cfa42ec5fc
adi yuda <adiyudapratama4@gmail.com>
2024-01-06 15:23:26 +0700

    bottom nav bar

---
+++ lib/screens/buttomNavBar.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth_demo/screens/home.dart';
+import 'package:flutter/material.dart';
+// import 'package:rent/ui/page/cart.dart';
+// import 'package:rent/ui/page/payment.dart';
+// import 'package:rent/ui/page/profile.dart';
+
+// import '../page/home.dart';
+
+class MyBottomNavigationBar extends StatefulWidget {
+  static String routeName = '/MyBottomNavigationBar';
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    // Cart(),
+    // ProfilePageV2(),
+    // PaymentPage()
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
